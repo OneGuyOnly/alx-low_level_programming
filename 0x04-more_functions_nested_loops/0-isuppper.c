6ef0ac62ef378701e2f839908e8fa4498685966f
Ezequiel <contact.icezekiel@gmail.com>
2023-07-21 18:09:40 -0700

    More functions and nested loops projects

---
+++ 0x04-more_functions_nested_loops/0-isuppper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - Entry Point
+ * Description: 'Check upper case'
+ * @n: integer input
+ * Return: 0 or 1
+ */
+int _isupper(int n)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
