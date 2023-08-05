eede50c010364ad5d6accde4da4a191b05c1de94
Ezequiel <contact.icezekiel@gmail.com>
2023-08-04 18:58:24 -0700

    argc, argv projects

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: 'print number of arguments'
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc)
+	return (0);
+}
